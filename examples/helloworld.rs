92f98b42fde79b2c64a9bc47e18509c04a7164ae
Deadbeef <ent3rm4n@gmail.com>
2021-10-07 14:47:57 +0000

    YES YES YES

--- examples/helloworld.rs
+++ examples/helloworld.rs
@@ -1,12 +1,18 @@
 use std::error::Error;
 
-use flow_sdk::client::TonicHyperFlowClient;
+use flow_sdk::{client::TonicHyperFlowClient, protobuf::access::Timestamp};
+
+use chrono::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let mut net = TonicHyperFlowClient::mainnet()?;
     let _ = net.ping().await?;
     let latest_block_header = net.latest_block_header(true).await?;
-    println!("{:?}", latest_block_header.into_inner().0.into_inner());
+    let block_header = latest_block_header.into_inner().0.into_inner();
+    println!("{:?}", block_header);
+    let Timestamp { nanos, seconds } = block_header.timestamp.into_inner();
+    println!("{}", Utc.timestamp(seconds, nanos as u32));
+
     Ok(())
 }
