b0c1457f5dcf3a7a05f93ad979b4a823266c7fda
Deadbeef <ent3rm4n@gmail.com>
2021-10-25 22:53:40 +0800

    Cadence_json refactor

--- cadence_json/src/lib.rs
+++ cadence_json/src/lib.rs
@@ -13,69 +13,7 @@ pub use fixed::*;
 pub mod de;
 pub mod ser;
 
-pub mod value;
-
-pub(crate) mod wrapper {
-    use std::str::FromStr;
-
-    /// A type in the cadence type system that needs to delegate serde implementation to a newtype wrapper.
-    ///
-    /// For example, integers are strings within the Cadence-JSON interchange format, so we need to use
-    /// the `FromStr` and `Display` implementations to (de)serialize instead.
-    ///
-    /// # Safety
-    ///
-    /// `Self::Wrapped` must have the same layout as `Self`, i.e. they are safely transmutable.
-    ///
-    /// Make sure that `Self::Wrapped` is a newtype struct annotated with #[repr(transparent)]
-    pub unsafe trait Wrap {
-        type Wrapped;
-    }
-
-    pub fn wrap<T: Wrap>(of: &T) -> &T::Wrapped {
-        // Safety: the wrapper is guarranteed to be #[repr(transparent)] over `T`.
-        unsafe { &*(of as *const T as *const T::Wrapped) }
-    }
-
-    macro_rules! wrapper {
-        ($Name:ident($ty:path)) => {
-            #[derive(serde_with::DeserializeFromStr, serde_with::SerializeDisplay)]
-            #[repr(transparent)]
-            pub struct $Name(pub $ty);
-
-            impl FromStr for $Name {
-                type Err = <$ty as FromStr>::Err;
-
-                fn from_str(s: &str) -> Result<Self, Self::Err> {
-                    s.parse().map($Name)
-                }
-            }
-
-            impl std::fmt::Display for $Name {
-                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                    std::fmt::Display::fmt(&self.0, f)
-                }
-            }
-
-            unsafe impl Wrap for $ty {
-                type Wrapped = $Name;
-            }
-        };
-    }
-
-    wrapper!(BigUint(num_bigint::BigUint));
-    wrapper!(BigInt(num_bigint::BigInt));
-    wrapper!(I8(i8));
-    wrapper!(I16(i16));
-    wrapper!(I32(i32));
-    wrapper!(I64(i64));
-    wrapper!(I128(i128));
-    wrapper!(U8(u8));
-    wrapper!(U16(u16));
-    wrapper!(U32(u32));
-    wrapper!(U64(u64));
-    wrapper!(U128(u128));
-}
+pub(crate) mod wrapper;
 
 #[derive(SerializeDisplay, DeserializeFromStr, Debug, PartialEq, Eq, Clone, Copy)]
 pub enum PathDomain {
@@ -157,7 +95,7 @@ pub struct EntryOwned {
 
 #[derive(DeserializeFromStr, Clone, PartialEq, Eq)]
 pub struct AddressOwned {
-    pub data: Vec<u8>,
+    pub data: Box<[u8]>,
 }
 
 impl FromStr for AddressOwned {
@@ -168,7 +106,7 @@ impl FromStr for AddressOwned {
             .strip_prefix("0x")
             .ok_or(Cow::Borrowed("Address does not start with 0x"))?;
         hex::decode(s)
-            .map(|data| AddressOwned { data })
+            .map(|data| AddressOwned { data: data.into_boxed_slice() })
             .map_err(|e| match e {
                 FromHexError::OddLength => Cow::Borrowed("Odd number of digits"),
                 FromHexError::InvalidStringLength => Cow::Borrowed("Invalid string length"),
---
+++ cadence_json/src/wrapper.rs
@@ -0,0 +1,59 @@
+use std::str::FromStr;
+
+/// A type in the cadence type system that needs to delegate serde implementation to a newtype wrapper.
+///
+/// For example, integers are strings within the Cadence-JSON interchange format, so we need to use
+/// the `FromStr` and `Display` implementations to (de)serialize instead.
+///
+/// # Safety
+///
+/// `Self::Wrapped` must have the same layout as `Self`, i.e. they are safely transmutable.
+///
+/// Make sure that `Self::Wrapped` is a newtype struct annotated with #[repr(transparent)]
+pub unsafe trait Wrap {
+    type Wrapped;
+}
+
+pub fn wrap<T: Wrap>(of: &T) -> &T::Wrapped {
+    // Safety: the wrapper is guarranteed to be #[repr(transparent)] over `T`.
+    unsafe { &*(of as *const T as *const T::Wrapped) }
+}
+
+macro_rules! wrapper {
+    ($Name:ident($ty:path)) => {
+        #[derive(serde_with::DeserializeFromStr, serde_with::SerializeDisplay)]
+        #[repr(transparent)]
+        pub struct $Name(pub $ty);
+
+        impl FromStr for $Name {
+            type Err = <$ty as FromStr>::Err;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                s.parse().map($Name)
+            }
+        }
+
+        impl std::fmt::Display for $Name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                std::fmt::Display::fmt(&self.0, f)
+            }
+        }
+
+        unsafe impl Wrap for $ty {
+            type Wrapped = $Name;
+        }
+    };
+}
+
+wrapper!(BigUint(num_bigint::BigUint));
+wrapper!(BigInt(num_bigint::BigInt));
+wrapper!(I8(i8));
+wrapper!(I16(i16));
+wrapper!(I32(i32));
+wrapper!(I64(i64));
+wrapper!(I128(i128));
+wrapper!(U8(u8));
+wrapper!(U16(u16));
+wrapper!(U32(u32));
+wrapper!(U64(u64));
+wrapper!(U128(u128));
