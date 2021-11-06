f6f6c4df17d93c34b2e3e62097e255c20edf2225
Deadbeef <ent3rm4n@gmail.com>
2021-11-07 01:38:47 +0800

    Revert examples=all

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -4,6 +4,6 @@ set -e
 
 git apply rename.diff
 
-cargo doc -Z unstable-options -Z rustdoc-scrape-examples=all || cargo doc -Z unstable-options -Z rustdoc-scrape-examples=all
+cargo doc -Z unstable-options -Z rustdoc-scrape-examples=examples
 
 git apply -R rename.diff
