74d8677aa53591e87273df606adb8941c139f554
Deadbeef <ent3rm4n@gmail.com>
2021-11-06 18:48:51 +0800

    Update make_docs to use =all fixed in nightly

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -2,6 +2,6 @@
 
 git apply rename.diff
 
-cargo doc -p flow_sdk -Z unstable-options -Z rustdoc-scrape-examples=examples
+cargo doc -p flow_sdk -Z unstable-options -Z rustdoc-scrape-examples=all
 
 git apply -R rename.diff
